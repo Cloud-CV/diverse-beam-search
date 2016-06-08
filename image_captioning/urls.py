627dd0406ab584ad68f494230536eaff19bb38c4
deshraj <deshrajdry@gmail.com>
2016-06-09 03:42:20 +0530

    media url added for debug mode of operation

--- image_captioning/urls.py
+++ image_captioning/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from image_captioning import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^beam_search/$', views.beam_search, name='beam_search'),
     url(r'^$', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
